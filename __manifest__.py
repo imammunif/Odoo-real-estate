b0657fbd29a7f12c71be9c508dcf709f38868a37
Imam Munif <imammunif17@gmail.com>
2024-09-20 15:51:12 +0700

    initiate the view directory also creating the xml views for the model

--- __manifest__.py
+++ __manifest__.py
@@ -11,14 +11,14 @@
     'application': True,
     #'depends': [],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
         # 'data/estate.property.type.csv',
         # 'data/estate.property.csv',
         # 'security/ir.model.access.csv',
         # 'views/estate_property_type_views.xml',
         # 'views/estate_property_tag_views.xml',
         # 'views/estate_property_offer_views.xml',
-        # 'views/estate_property_views.xml',
+        'views/estate_property_views.xml',
         #
         # 'views/estate_menus.xml'
     ],
