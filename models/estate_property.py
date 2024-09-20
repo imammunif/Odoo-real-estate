a01a062f46f0b21acb85073b39ceb3b7235fe364
Imam Munif <imammunif17@gmail.com>
2024-09-20 20:57:50 +0700

    set default computed value for date_availability field

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,13 +1,18 @@
-from odoo import fields, models, _
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate property description"
 
-    name = fields.Char(string="Name", required=True)
+    def _default_date_availability(self):
+        return fields.Date.context_today(self) + relativedelta(months=3)
+
+    name = fields.Char(string="Title", required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Date Availability', copy=False)
+    date_availability = fields.Date(string='Date Availability', default=lambda self: self._default_date_availability(), copy=False)
     expected_price = fields.Float(string='Expected Price', required=True)
     selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
     bedrooms = fields.Integer(string='Bedrooms', default=2)
