4af864ec8ac91a40724cff69172a09cb799fb716
Imam Munif <imammunif17@gmail.com>
2024-09-20 12:06:37 +0700

    initiating project

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Estate",
+    'summary': """Real estate management""",
+    'description': """
+        Real estate management module
+    """,
+    'license': 'LGPL-3',
+    'category': 'Administration',
+    'version': '0.1',
+    'application': True,
+    #'depends': [],
+    'data': [
+        # 'data/estate.property.type.csv',
+        # 'data/estate.property.csv',
+        # 'security/ir.model.access.csv',
+        # 'views/estate_property_type_views.xml',
+        # 'views/estate_property_tag_views.xml',
+        # 'views/estate_property_offer_views.xml',
+        # 'views/estate_property_views.xml',
+        #
+        # 'views/estate_menus.xml'
+    ],
+    #'demo': [
+    #'demo.xml',
+    #],
+}
