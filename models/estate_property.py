a10573aff89ea959b44ba06a9641f26c306fc4b9
Imam Munif <imammunif17@gmail.com>
2024-09-24 10:49:51 +0700

    feat: add sql constraint for expected_price

--- models/estate_property.py
+++ models/estate_property.py
@@ -9,6 +9,10 @@ from odoo.odoo.exceptions import UserError
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate property description"
+    _sql_constraints = [
+        # (name, sql_definition, message)
+        ("check_expected_price", "CHECK(expected_price > 0)", "The expected price should be strictly positive")
+    ]
 
     def _default_date_availability(self):
         return fields.Date.context_today(self) + relativedelta(months=3)
